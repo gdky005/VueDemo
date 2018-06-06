3be1fe00ea5e0d94214278979113115b9a313f19
qingw <qingw@jumei.com>
2018-06-06 11:44:18 +0800

    1. 初始化 VUE 的项目； 2. 添加 自我测试的 页面。

---
+++ src/components/WangQing.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="wq">
+
+    <b>{{ name }}</b>
+    <layer v-for="name in names" :key="name" v-bind:item="name"/>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "WangQing",
+    data() {
+      return {
+        name: "WangQing_VUE!",
+        names: [
+          { name: 'W'},
+          { name: 'Q'},
+          { name: 'L'},
+        ]
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
